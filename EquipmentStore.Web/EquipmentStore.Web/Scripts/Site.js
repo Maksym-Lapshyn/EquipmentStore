2709dda7fe77a9bcfb4e05106ae84085b7e62f1c
maxon-adc <lapshyn.maksym@gmail.com>
2017-10-20 19:14:18 +0300

    did something

---
+++ EquipmentStore.Web/EquipmentStore.Web/Scripts/Site.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    var url = window.location;
+
+    // Marks equipment dropdown as active
+    if (url.toString().includes('products/category'))
+    {
+        $('.equipment-dropdown').addClass('active');
+        return;
+    }
+
+    // Will only work if string in href matches with location
+    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+
+    // Will also work for relative and absolute hrefs
+    $('ul.nav a').filter(function () {
+        return this.href == url;
+    }).parent().addClass('active').parent().parent().addClass('active');
+});
